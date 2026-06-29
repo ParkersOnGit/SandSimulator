a459a02d915daab66757ed4ed4de1d59f6bee296
Parker H <geodasher10@gmail.com>
2026-06-29 13:31:26 -0500

    Small code cleanup.

--- CellularAutomata/main.cpp
+++ CellularAutomata/main.cpp
@@ -3,21 +3,9 @@
 #include <thread>
 #include <chrono>
 #include <conio.h> // Windows specific.
+#include <random>
 
 const int WIDTH = 25, HEIGHT = 25;
-
-namespace Color {
-	static const std::string RESET = "\033[0m";
-	static const std::string BLACK = "\033[40m";
-	static const std::string RED = "\033[41m";
-	static const std::string GREEN = "\033[42m";
-	static const std::string YELLOW = "\033[43m";
-	static const std::string BLUE = "\033[44m";
-	static const std::string PURPLE = "\033[45m";
-	static const std::string CYAN = "\033[46m";
-	static const std::string WHITE = "\033[47m";
-	static const std::string GRAY = "\033[100m";
-};
 
 std::string buffer;
 std::array<std::array<int, WIDTH>, HEIGHT> board;
@@ -26,20 +14,26 @@ std::array<int, 2> cursorPosition = {(int)(WIDTH / 2), 1};
 void printBoard() {
 	buffer = "";
 
-	for (int y = 1; y <= HEIGHT; y++) {
-		for (int x = 1; x <= WIDTH; x++) {
-			if (x == cursorPosition[0] && y == cursorPosition[1]) {
-				buffer += Color::RED;
-				buffer += "  ";
-				continue;
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			if (x == cursorPosition[0] - 1 && y == cursorPosition[1] - 1) {
+				buffer += "\033[48;2;150;10;10m";
+			} 
+			else if (board[y][x] == 0) {
+				buffer += x % 2 - y % 2 == 0 ? "\033[48;2;100;100;100m" : "\033[48;2;95;95;95m";
 			}
-			buffer += (board[y - 1][x - 1] == 0 ? Color::GRAY : Color::YELLOW);
+			else {
+				std::mt19937 gen(x + 1 << y);
+				std::uniform_int_distribution<int> distr(0, 15);
+				buffer += "\033[48;2;" + std::to_string(200 - distr(gen)) + ";" + std::to_string(180 - distr(gen)) + ";130m";
+			}
 			buffer += "  ";
 		}
 		buffer += "\n";
 	}
 
-	std::cout << "\033[H";
+	// Return home and print buffer.
+	std::cout << "\033[H"; 
 	std::cout << buffer << std::flush;
 }
 
@@ -47,25 +41,24 @@ void updateBoard() {
 	for (int y = HEIGHT - 2; y > -1; y--) {
 		for (int x = 0; x < WIDTH; x++) {
 			if (board[y][x] != 1) continue;
-			if (board[y + 1][x] == 1) {
-				
-				
-				if (x != WIDTH - 1 && board[y + 1][x + 1] == 0) {
-					// Move to the right.
-					board[y][x] = 0;
-					board[y + 1][x + 1] = 1;
-				}
-				else if (x != 0 && board[y + 1][x - 1] == 0) {
-					// Move to the left.
-					board[y][x] = 0;
-					board[y + 1][x - 1] = 1;
-				}
-				
+
+			if (board[y + 1][x] != 1) {
+				board[y][x] = 0;
+				board[y + 1][x] = 1;
 				continue;
 			}
 
-			board[y][x] = 0;
-			board[y + 1][x] = 1;
+			if (x != WIDTH - 1 && board[y + 1][x + 1] == 0) {
+				// Move to the right.
+				board[y][x] = 0;
+				board[y + 1][x + 1] = 1;
+			}
+
+			else if (x != 0 && board[y + 1][x - 1] == 0) {
+				// Move to the left.
+				board[y][x] = 0;
+				board[y + 1][x - 1] = 1;
+			}
 		}
 	}
 }
@@ -93,7 +86,6 @@ void getInput() {
 }
 
 int main() {
-	
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
 	// Initial board print.
@@ -107,6 +99,5 @@ int main() {
 		std::this_thread::sleep_for(std::chrono::milliseconds(50));
 	}
 
-	std::cout << Color::RESET;
 	return 0;
 }
